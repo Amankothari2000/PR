f51efcb4ae3311b65fee7b82578be987b5af283a
AmanKothari20 <56184181+AmanKothari20@users.noreply.github.com>
2023-10-06 19:26:38 +0530

    Update add.java

--- add.java
+++ add.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class add{
 	public static void main(String[] args){
 		int a,b;
-		
+
+		//Taking user input
 		Scanner sc = new Scanner(System.in);
 		a=sc.nextInt();
 		b=sc.nextInt();
